32588a3f3d235e7b9c248b090dec3523d1a56d5e
witha <witha@DESKTOP-G20EEGB>
2024-05-06 09:21:36 +0530

    Add project files.

---
+++ WebAPIPOS/Controllers/ShirtsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIPOS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShirtsController : ControllerBase
+    {
+    }
+}
---
+++ WebAPIPOS/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+//Routing
+// "/shirts"
+app.MapGet("/shirts", () => {
+    return "Reading all the shirts";
+});
+
+app.MapGet("/shirts/{id}", (int id) => {
+    return $"Reading shirt with ID:{id}";
+});
+
+app.MapPost("/shirts", () =>
+{
+    return "Creating a shirt.";
+});
+
+app.MapPut("/shirts/{id}", (int id) => {
+    return $"Updating shirt with ID:{id}";
+});
+
+app.MapDelete("/shirts/{id}",(int id)=>{
+    return $"Deleting shirt with ID:{id}";
+});
+
+app.Run();
+
